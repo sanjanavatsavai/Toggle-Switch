56e51aae945c276ce2e05d8e2e2d436dbbd53ec7
sanjanavatsavai <sanju82@outlook.com>
2025-05-29 10:45:11 -0400

    Ghost animations

--- toggle-switch/src/ToggleSwitch.js
+++ toggle-switch/src/ToggleSwitch.js
@@ -8,15 +8,31 @@ function ToggleSwitch() {
 const ToggleSwitch = () => {
     setIsDark(!isDark);
 };
+
+const statusMessage = isDark ? "💀 Ghosts come out to play!"
+    : "👻 No ghosts here! ";
       return (
     <div className={`app-container ${isDark ? 'dark' : 'light'}`}>
+        { isDark && (
+      <>
+        <div className="ghost-tl">👻</div>
+        <div className="ghost-tr">👻</div>
+        <div className="ghost-bl">👻</div>
+        <div className="ghost-br">👻</div>
+        <div className="ghost-top">👻</div>
+        <div className="ghost-bottom">👻</div>
+        <div className="ghost-left-center">👻</div>
+        <div className="ghost-right-center">👻</div>
+      </>
+    )}
+        <h1 className="status-text">{statusMessage}</h1>
         <div className="toggle-switch" onClick={ToggleSwitch}>
             <div className={`slider ${isDark ? 'slider-dark' : 'slider-light'}`}>
                 {isDark ? '🌙' : '☀️'}
 
             </div>
-
         </div>
+         { !isDark && <p className="dare-text">Slide it, I dare you.</p> }
     </div>
   )
 }
